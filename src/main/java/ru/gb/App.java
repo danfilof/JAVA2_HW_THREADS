49b7182e03c9bc865b213d091ea641bc60c305cb
danfilof <danila.anfilofyev@gmail.com>
2022-01-23 12:37:19 +0100

    Technically HW is completed but ArrayIndexOutOfBoundsException exists.

--- src/main/java/ru/gb/App.java
+++ src/main/java/ru/gb/App.java
@@ -6,11 +6,14 @@ public class App {
     public static void main(String[] args) {
 
         final int size = 10000000;
-         final int half = size / 2;
+         final int h = size / 2;
         float[] arr = new float[size];
 
         method1(arr);
+        System.out.println(" ");
+        method2(arr, h, size);
     }
+
 
     public static void method1(float[] arr) {
         Arrays.fill(arr, 1);
@@ -23,4 +26,60 @@ public class App {
         long result = System.currentTimeMillis() - a;
         System.out.printf("Однопоточное вычисление. Результат: %s", result);
     }
+
+
+    public static class MyThread extends Thread {
+
+        private float[] a;
+        private int start, end;
+
+        MyThread(float[] a, int start, int end) {
+            this.a = a;
+            this.start = start;
+            this.end = end;
+        }
+
+        @Override
+        public void run() {
+            for (int i = start; i < end; i++) {
+                a[i] = (float)(a[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
+            }
+        }
+    }
+
+
+    public static void method2(float[] arr, int h, int size) {
+
+        Arrays.fill(arr,1);
+
+        long a = System.currentTimeMillis();
+        float[] arr1 = new float[h];
+        float[] arr2 = new float[h];
+
+
+        System.arraycopy(arr, 0, arr1, 0, h);
+        System.arraycopy(arr, h, arr2, 0, h);
+
+        MyThread myThread1 = new MyThread(arr1, 0, h-1);
+        MyThread myThread2 = new MyThread(arr2, h, size);
+
+        myThread1.start();
+        myThread2.start();
+
+        try {
+            myThread1.join();
+            myThread1.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.arraycopy(arr1, 0, arr, 0, h);
+        System.arraycopy(arr2, 0, arr, h, h);
+
+        System.currentTimeMillis();
+        long result = System.currentTimeMillis() - a;
+        System.out.printf("Многопоточное вычисление. Результат: %s",result);
+
+    }
+
 }
