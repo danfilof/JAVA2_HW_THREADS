3e9772838cf33a468b35fa7ee2799a075c93e86b
danfilof <danila.anfilofyev@gmail.com>
2022-01-23 16:52:05 +0100

    HW completed

--- src/main/java/ru/gb/App.java
+++ src/main/java/ru/gb/App.java
@@ -6,64 +6,59 @@ public class App {
     public static void main(String[] args) {
 
         final int size = 10000000;
-         final int h = size / 2;
+        final int h = size / 2;
         float[] arr = new float[size];
 
         method1(arr);
-        System.out.println(" ");
+        System.out.println(" \n");
         method2(arr, h, size);
     }
 
-
     public static void method1(float[] arr) {
         Arrays.fill(arr, 1);
-
-        long a = System.currentTimeMillis();
+        long a1 = System.currentTimeMillis();
         for (int i = 0; i < arr.length; i++) {
             arr[i] = (float)(arr[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
         }
-        System.currentTimeMillis();
-        long result = System.currentTimeMillis() - a;
+        long result = System.currentTimeMillis() - a1;
         System.out.printf("Однопоточное вычисление. Результат: %s миллисекунд.", result);
     }
-
 
     public static class MyThread extends Thread {
 
         private float[] a;
-        private int start, end;
+        private int iLen;
 
-        MyThread(float[] a, int start, int end) {
+        MyThread(float[] a, int iLen) {
             this.a = a;
-            this.start = start;
-            this.end = end;
+            this.iLen = iLen;
         }
 
         @Override
         public void run() {
-            for (int i = start; i < end; i++) {
+            for (int i = 0; i < iLen; i++) {
                 a[i] = (float)(a[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
             }
         }
     }
 
-
     public static void method2(float[] arr, int h, int size) {
 
         Arrays.fill(arr,1);
 
-        long a = System.currentTimeMillis();
+        long a2 = System.currentTimeMillis();
+
         float[] arr1 = new float[h];
         float[] arr2 = new float[h];
-
 
         System.arraycopy(arr, 0, arr1, 0, h);
         System.arraycopy(arr, h, arr2, 0, h);
 
-        MyThread myThread1 = new MyThread(arr1, 0, h-1);
-        MyThread myThread2 = new MyThread(arr2, h, size);
+        MyThread myThread1 = new MyThread(arr1, h);
+        MyThread myThread2 = new MyThread(arr2, h);
+
         myThread1.setDaemon(true);
-        myThread2.setDaemon(false);
+        myThread2.setDaemon(true);
 
         myThread1.start();
         myThread2.start();
@@ -79,8 +74,8 @@ public class App {
         System.arraycopy(arr2, 0, arr, h, h);
 
         System.currentTimeMillis();
-        long result = System.currentTimeMillis() - a;
-        System.out.printf("Многопоточное вычисление. Результат: %s миллисекунд.",result);
+        long result = System.currentTimeMillis() - a2;
+        System.out.printf("Многопоточное вычисление. Результат: %s миллисекунд.\n",result);
 
     }
 
